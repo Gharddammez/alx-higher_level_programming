75635917ae14bcc083fe8da22031cd318af765cc
Gharddammez <guavgharddammez@gmail.com>
2023-08-09 15:50:16 +0300

    Updated files in 0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author -Godswill Kalu
+# Author - Jafred Anemba
 
 def pow(a, b):
     return (a ** b)
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
+# Author - Jafred Anemba
 
 def islower(c):
     """Function checks for lowercase characters."""
